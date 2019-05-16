f8b861818c350b69e40de9da658dfabc2010c341
Jane Connolly <janeconnolly@gmail.com>
2019-05-15 23:54:10 -0400

    Select final hero banner, fix issue with ACF

--- wp-content/themes/understrap-child/partials/aate-hero.php
+++ wp-content/themes/understrap-child/partials/aate-hero.php
@@ -13,40 +13,29 @@ $button_link_title = get_field('hero_button_link_title');
 
 ?>
 <!-- Hero -->
-<div class="aate-hero-three mb-4" role="banner">
-    <div class="container pt-7 pb-6">
-
+<section class="aate-hero" role="banner">
+    <div class="container">
         <div class="row">
-            <div class="col-md" style="padding-bottom: 2rem; padding-top: 2rem;">
 
+            <?php if($image): ?>
+                <div class="aate-hero_image">
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" title="<?php echo $image['title']; ?>" >
+                </div>
+                <div class="col-md-6">
+            <?php else : ?>
+                <div class="col-md-10">
+            <?php endif; ?>
+            
                 <h1><?php the_title(); ?></h1>
-
                 <?php if($intro): ?>
                     <p class="lead"><?php echo $intro ?></p>
                 <?php endif; ?>
-
                 <?php if($button_include == true): ?>
-                    <a href="<?php echo $button_link; ?>" class="btn btn-outline-primary" title="<?php echo $button_title; ?>" <?php if($button_link_type == 'External'): ?>target="_blank"<?php endif; ?>><?php echo $button_text; ?></a>
+                    <a href="<?php echo $button_link; ?>" class="btn btn-outline-dark" title="<?php echo $button_title; ?>" <?php if($button_link_type == 'External'): ?>target="_blank"<?php endif; ?>><?php echo $button_text; ?></a>
                 <?php endif; ?>
             </div>
 
-            <?php if($image): ?>
-                <div class="col-md">
-
-                    <div class="skew-wrapper">
-                        <div class="skew-3">
-                            <div class="unskew-3" role="img"
-                                    aria-label="<?php echo $image['alt']; ?>"
-                                    title="<?php echo $image['title']; ?>" 
-                                    style="background-image: url('<?php echo $image['url']; ?>');"></div>
-                            <!--<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />-->
-                        </div>
-                    </div>
-
-                </div>
-            <?php endif; ?>
         </div>
-
     </div>
-</div>
+</section>
 <!-- /Hero -->
